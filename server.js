d652eecf38ff3a4d4590e64e372d3be56298e78c
rusomsumsam <sbintemostofa@gmail.com>
2026-06-16 07:50:43 +0600

    Use BigInt for large LCM values

--- server.js
+++ server.js
@@ -4,33 +4,34 @@ function isNatural(n) {
     return /^[1-9]\d*$/.test(n);
 }
 
-function gcd(a, b) { 
-    while (b !== 0) {
+function gcd(a, b) {
+    while (b !== 0n) {
         const temp = b;
         b = a % b;
         a = temp;
     }
     return a;
-};
+}
 
 function lcm(a, b) {
-    return (a * b) / gcd(a, b);
-};
+    return (a / gcd(a, b)) * b;
+}
 
 const server = http.createServer((req, res) => {
     const url = new URL(req.url, `http://${req.headers.host}`);
     const path = url.pathname;
     const x = url.searchParams.get('x');
     const y = url.searchParams.get('y');
-    
-    res.writeHead(200, { "content-type": "text/plain" });
-    if (path == '/sbintemostofa_gmail_com') {
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    if (path === '/sbintemostofa_gmail_com') {
         if (!isNatural(x) || !isNatural(y)) {
             res.end('NaN');
         } else {
-            const a = Number(x);
-            const b = Number(y);
-            res.end(String(lcm(a, b)));
+            const a = BigInt(x);
+            const b = BigInt(y);
+            res.end(lcm(a, b).toString());
         }
     } else {
         res.end('NaN');
